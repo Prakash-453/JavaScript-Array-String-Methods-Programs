68f98a86e7674fa2d3067c73e5b3625984a4b1d7
Prakash-453 <alwaysprakash453@gmail.com>
2024-09-08 19:20:06 +0530

    Javascript Array-String-Methods Programs

--- Array-String-Methods-Programs.js
+++ Array-String-Methods-Programs.js
@@ -17,8 +17,7 @@ dlrow olleh
 
 2) Find the Longest Word in a Sentence 
 
-const inputSentence = "This is a sentence with words of 
-                                          varying lengths"; 
+const inputSentence = "This is a sentence with words of varying lengths"; 
 Output: "sentence"
 
 PROGRAM:
@@ -40,8 +39,7 @@ sentence
 
 3: Count the Occurrences of a Word in a Sentence ?
 
-const inputSentence = "This sentence repeats the word 
-                                       'this' twice"; 
+const inputSentence = "This sentence repeats the word 'this' twice"; 
 Output: 2
 
 const inputSentence = "This sentence repeats the word 'this' twice";
@@ -62,8 +60,7 @@ OUTPUT
    2
 4) Capitalize the First Letter of Each Word  ?
 
-const inputSentence = "this is a sentence with words to 
-                                       capitalize"; 
+const inputSentence = "this is a sentence with words to capitalize"; 
 Output: "This Is A Sentence With Words To Capitalize"
 
 PROGRAM
@@ -78,7 +75,7 @@ This Is A Sentence With Words To Capitalize
 5: Remove Duplicate Elements from an Array ?
 
 const inputArray = [1, 2, 3, 2, 4, 1];
- Output: [1, 2, 3, 4]
+Output: [1, 2, 3, 4]
 
 PROGRAM
 const inputArray = [1, 2, 3, 2, 4, 1];
